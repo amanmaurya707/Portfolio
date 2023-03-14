687181d4b7f0462fe0d94d1c692e4b47a8a29fd4
AMANKUMAR SURENDRAPRATAP MAURYA <amanmaurya707@gmail.com>
2023-03-14 21:00:33 +0530

    files added

---
+++ download_cv.js
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+//function downloadCVPDF()
+//{
+
+$(document).ready(function(){
+
+  $("#clickDownload_CV").click(function(){
+       
+    alert("Official Use Only!!!fine");
+   var restorePage=$("#view_profile_data").html();
+   alert(restorePage);
+   var printContent=$("#download_cv").html();
+   alert(printContent);
+   document.body.innerHTML=printContent;
+   //alert("@@");
+   window.print();
+   document.body.innerHTML=restorePage;
+  
+});
+});
+=======
+//function downloadCVPDF()
+//{
+
+$(document).ready(function(){
+
+  $("#clickDownload_CV").click(function(){
+       
+    alert("Official Use Only!!!fine");
+   var restorePage=$("#view_profile_data").html();
+   alert(restorePage);
+   var printContent=$("#download_cv").html();
+   alert(printContent);
+   document.body.innerHTML=printContent;
+   //alert("@@");
+   window.print();
+   document.body.innerHTML=restorePage;
+  
+});
+});
+>>>>>>> master
+//}
